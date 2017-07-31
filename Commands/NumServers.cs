5c8624fa1675cde5bda778942bd4a9c05e3222b1
Michael Flaherty <michaelwflaherty@me.com>
2017-07-30 22:00:25 -0700

    Update Steam Server Query

--- Commands/NumServers.cs
+++ Commands/NumServers.cs
@@ -22,7 +22,7 @@ namespace ChancyBot.Commands
             var request = new CGameServers_GetServerList_Request
             {
                 filter = Program.Instance.connection.filter,
-                limit = 50000,
+                limit = 20000, // max allowed
             };
 
             var callback2 = await Program.Instance.connection.GameServers.SendMessage(api => api.GetServerList(request));
