8738cb08fcd6d18a7195affbf3a2477673994aa7
assyrianic <assyrianic@users.noreply.github.com>
2017-08-29 22:49:00 -0500

    making changes headline wants
    changed string comparators to use the 'Equals' method.
    changed class name to avoid name conflicts.

--- Commands/Dice.cs
+++ Commands/Dice.cs
@@ -11,22 +11,22 @@ using System.Linq;
 
 namespace ChancyBot.Commands
 {
-	public class HelpCommand : ModuleBase
+	public class DiceCommand : ModuleBase
 	{
 		[Command("roll"), Summary("rolls a dice of arbitrary size.")]
 		public async Task Say(string arg)
 		{
 			Random rand = new Random();
 			int num = 0;
-			if( arg == "d6" )
+			if( arg.Equals("d6") )
 				num = rand.Next(1, 6);
-			else if( arg == "d10" )
+			else if( arg.Equals("d10") )
 				num = rand.Next(1, 10);
-			else if( arg == "d20" )
+			else if( arg.Equals("d20") )
 				num = rand.Next(1, 20);
-			else if( arg == "d50" )
+			else if( arg.Equals("d50") )
 				num = rand.Next(1, 50);
-			else if( arg == "d100" )
+			else if( arg.Equals("d100") )
 				num = rand.Next(1, 100);
 			
 			await Context.Channel.SendMessageAsync("you rolled a "+num);
