2843b837dcc0d5e348b9140c0ef4fbbec0063fca
Michael Flaherty <michaelwflaherty@me.com>
2017-08-31 17:41:57 -0700

    Start 1.1 construction

--- Commands/Dice.cs
+++ Commands/Dice.cs
@@ -13,23 +13,31 @@ namespace ChancyBot.Commands
 {
 	public class DiceCommand : ModuleBase
 	{
-		[Command("roll"), Summary("rolls a dice of arbitrary size. (d6, d10, d20, d50, d100)")]
+        private Random rand;
+
+        public DiceCommand()
+        {
+            rand = new Random();
+        }
+
+        [Command("roll"), Summary("Rolls a dice of arbitrary size.")]
 		public async Task Say(string arg)
 		{
-			Random rand = new Random();
-			int num = 0;
-			if( arg.Equals("d6") )
-				num = rand.Next(1, 6);
-			else if( arg.Equals("d10") )
-				num = rand.Next(1, 10);
-			else if( arg.Equals("d20") )
-				num = rand.Next(1, 20);
-			else if( arg.Equals("d50") )
-				num = rand.Next(1, 50);
-			else if( arg.Equals("d100") )
-				num = rand.Next(1, 100);
-			
-			await Context.Channel.SendMessageAsync("you rolled a "+num);
-		}
+            int sideAmmount = ExtractDigits(arg);
+
+            int num = rand.Next(1, sideAmmount);
+
+            await Context.Channel.SendMessageAsync("You rolled a " + num + "!");
+        }
+
+        public static int ExtractDigits(string input)
+        {
+            char[] characters = input.Where(c => Char.IsNumber(c)).ToArray();
+            string digits = new string(characters);
+
+            int.TryParse(digits, out int result);
+
+            return result;
+        }
 	}
 }
---
+++ JobManager/Jobs/SelfUpdateListener.cs
@@ -0,0 +1,63 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+using System.Net;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ChancyBot.Jobs
+{
+    public class SelfUpdateListener : Job
+    {
+        CommitInfo commit;
+        string url;
+        string target;
+
+        public SelfUpdateListener(string url, string target)
+        {
+            this.target = target;
+            this.url = url;
+        }
+
+        public string GetRepo()
+        {
+            string[] split = url.Split('/');
+
+            return split[3] + "/" + split[4];
+        }
+
+        public override void OnRun()
+        {
+            try
+            {
+                string xml = new WebClient().DownloadString(this.url);
+                XDocument doc = XDocument.Parse(xml);
+                XNamespace ns = "http://www.w3.org/2005/Atom/";
+
+                var feed = doc.Descendants().ElementAt(0).Descendants().Elements();
+
+                CommitInfo current = new CommitInfo(feed);
+
+                if (commit == null) // first fetch
+                {
+                    commit = current;
+                }
+                else
+                {
+                    if (!commit.Equals(current))
+                    {
+                        int pid = Process.GetCurrentProcess().Id;
+                        // TODO Call Python Script With Latest Zip File
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Program.Instance.Log(new LogMessage(LogSeverity.Error, "SelfUpdateListener", ex.Message));
+            }
+        }
+    }
+}
