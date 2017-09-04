4bf0c1bc346b912ddc97c941791895f862ec817b
Michael Flaherty <michaelwflaherty@me.com>
2017-09-04 00:00:36 -0700

    Add manual update cmd (pre #9)

---
+++ Commands/Update.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+using Discord.Commands;
+
+using System.Diagnostics;
+using Octokit;
+using System.IO;
+
+namespace ChancyBot.Commands
+{
+    public class UpdateCommand : ModuleBase
+    {
+        [Command("update"), Summary("Updates and reloads the bot.")]
+        public async Task Say()
+        {
+            int pid = Process.GetCurrentProcess().Id;
+
+            var client = new GitHubClient(new ProductHeaderValue("my-cool-app"));
+            var releases = await client.Repository.Release.GetAll("Headline22", "Steam-Discord-Bot");
+
+            string url = "https://github.com/Headline22/Steam-Discord-Bot/releases/download/<name>/steam-discord-bot.zip";
+            url = url.Replace("<name>", releases[0].Name);
+
+            Context.Channel.SendMessageAsync("Okay. Updating...");
+
+            string command = string.Format("/k cd {0} & python updater.py {1} {2}",
+                                                            Directory.GetCurrentDirectory(),
+                                                            pid,
+                                                            url);
+
+            Process.Start("CMD.exe", command);
+            Environment.Exit(0);
+        }
+    }
+}
