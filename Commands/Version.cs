d5c9dc5d62a6527e63a964fe627131951d2e7215
Headline22 <michaelwflaherty@me.com>
2018-01-31 17:14:37 -0800

    NPOTB: Add version replace script & Version command

---
+++ Commands/Version.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+using Discord.Commands;
+using Octokit;
+
+using System.Diagnostics;
+using Octokit;
+using System.IO;
+
+namespace ChancyBot.Commands
+{
+    public class VersionCommand : ModuleBase
+    {
+        [Command("version"), Summary("Get the current bot version.")]
+        public async Task Say()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("Steam-Discord-Bot"));
+            var releases = await client.Repository.Release.GetAll("Headline", "Steam-Discord-Bot");
+            string version = releases[0].Name.Substring(releases[0].Name.IndexOf("-v"));
+            await Context.Channel.SendMessageAsync("Current version: " + Program.VERSION + " (latest is " + version + ")");
+        }
+    }
+}
