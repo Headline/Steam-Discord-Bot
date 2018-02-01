58ad57bb2f37cd05caf88855b63066fbdc1b57bb
Headline22 <michaelwflaherty@me.com>
2018-01-31 17:50:18 -0800

    Fix brainless mistake

--- Commands/Version.cs
+++ Commands/Version.cs
@@ -20,7 +20,7 @@ namespace ChancyBot.Commands
             var releases = await client.Repository.Release.GetAll("Headline", "Steam-Discord-Bot");
 
             var detectionString = "-v";
-            string version = releases[0].Name.Substring(releases[0].Name.IndexOf(detectionString)) + detectionString.Length;
+            string version = releases[0].Name.Substring(releases[0].Name.IndexOf(detectionString) + detectionString.Length);
 
             await Context.Channel.SendMessageAsync("Current version: " + Program.VERSION + " (latest is " + version + ")");
         }
