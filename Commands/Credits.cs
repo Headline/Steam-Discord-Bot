d73c467f667e5db158502bd0da61b28c2d09162a
Michael Flaherty <michaelwflaherty@me.com>
2018-04-28 14:03:24 -0700

    Add credits command

---
+++ Commands/Credits.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Octokit;
+
+
+namespace SteamDiscordBot.Commands
+{
+    public class CreditsCommand : ModuleBase
+    {
+        [Command("credits"), Summary("Outputs the credits for the bot, ordered by contributions")]
+        public async Task Say()
+        {
+            string owner;
+            string repo;
+
+            owner = Program.config.GitHubUpdateUsername;
+            repo = Program.config.GitHubUpdateRepository;
+            var contribs = await Program.Instance.ghClient.Repository.GetAllContributors(owner, repo);
+
+
+            List<RepositoryContributor> creators = new List<RepositoryContributor>(contribs);
+            creators.Sort(delegate (RepositoryContributor a, RepositoryContributor b)
+            {
+                if (a.Contributions == b.Contributions) return 0;
+                return (a.Contributions < b.Contributions) ? -1 : 1;
+            });
+
+            string output = "";
+            foreach (var usr in creators)
+            {
+                output += string.Format("{0} ({1} contribution(s))\n", usr.Login, usr.Contributions);
+            }
+
+            await Context.Channel.SendMessageAsync("Credits ordered by contribution count: \n" + output);
+        }
+    }
+}
